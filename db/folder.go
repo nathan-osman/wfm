f30a2684a78eff3d59e99233b6d807db1044d01c
Nathan Osman <nathan@quickmediasolutions.com>
2024-01-31 01:07:24 -0800

    Add parent folders.

--- db/folder.go
+++ db/folder.go
@@ -12,4 +12,6 @@ type Folder struct {
 	Description string    `gorm:"not null" json:"description"`
 	CanBrowse   bool      `gorm:"not null" json:"can_browse"`
 	CreatedAt   time.Time `gorm:"not null" json:"created_at"`
+	ParentID    int64     `gorm:"not null" json:"-"`
+	Parent      *Folder   `gorm:"constraint:OnDelete:CASCADE" json:"parent"`
 }
