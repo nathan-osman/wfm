ba0c0269c790c577ffe7fe389a2cd801fe08051f
Nathan Osman <nathan@quickmediasolutions.com>
2024-01-31 21:53:11 -0800

    Add empty React project for admin page.

---
+++ admin/src/index.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    Hello, world!
+  </>
+)
---
+++ admin/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+}
