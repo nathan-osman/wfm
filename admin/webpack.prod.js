73e4c449395d47fc8cd0f8ba3c2671d65e3f9384
Nathan Osman <nathan@quickmediasolutions.com>
2024-01-31 22:36:32 -0800

    Remove comment file.

--- admin/webpack.prod.js
+++ admin/webpack.prod.js
@@ -1,9 +1,23 @@
 const { merge } = require('webpack-merge')
+const TerserPlugin = require("terser-webpack-plugin")
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     clean: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
   },
 })
