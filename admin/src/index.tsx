6fd5b3ef43099cda2899486afeb61994bb34387e
Nathan Osman <nathan@quickmediasolutions.com>
2024-02-03 21:06:34 -0800

    Add Font Awesome for icons.

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom/client'
 import { ApiProvider } from './lib/api'
 import Root from './components/Root'
 import '@fontsource/montserrat'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!!).render(
