a3477c4f282c66b2d52a3c587702cb86ea6db4a8
Nathan Osman <nathan@quickmediasolutions.com>
2024-02-03 21:01:01 -0800

    Add Zod type definitions for errors and folders.

---
+++ admin/src/types/folders.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const FolderSchema = z.object({
+  id: z.string(),
+  uuid: z.string(),
+  name: z.string(),
+  description: z.string(),
+  created_at: z.string(),
+  type: z.string(),
+})
+
+export type Folder = z.TypeOf<typeof FolderSchema>
